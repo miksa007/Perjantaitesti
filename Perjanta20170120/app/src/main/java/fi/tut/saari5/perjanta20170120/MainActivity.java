ce5cc71aaa1d319122ef5c4b7fdfd56b9e5d44cd
miksa <miksa007@gmail.com>
2017-01-20 14:09:42 +0200

    Working basic example

--- Perjanta20170120/app/src/main/java/fi/tut/saari5/perjanta20170120/MainActivity.java
+++ Perjanta20170120/app/src/main/java/fi/tut/saari5/perjanta20170120/MainActivity.java
@@ -1,13 +1,23 @@
 package fi.tut.saari5.perjanta20170120;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    public final static String EXTRA_MESSAGE="fi.tut.saari5.perjantai20170120.MESSAGE";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void sendMessage(View view){
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText =(EditText)findViewById(R.id.edit_message);
+        String message =editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 }
